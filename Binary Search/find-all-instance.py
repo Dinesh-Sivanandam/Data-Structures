0473e289cfccde59f2fef55604c6ff3866cd4e73
Dinesh <61511078+Dinesh-Sivanandam@users.noreply.github.com>
2020-10-17 13:12:12 +0530

    Add files via upload

---
+++ Binary Search/find-all-instance.py
@@ -0,0 +1,48 @@
+def binary_search(numbers_list, number_to_find):
+    left_index = 0
+    right_index = len(numbers_list) - 1
+    mid_index = 0
+
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
+
+        if mid_number == number_to_find:
+            return mid_index
+
+        if mid_number < number_to_find: # this means number is in right hand side of the list
+            left_index = mid_index + 1
+        else: # number to find is on left hand side of the list
+            right_index = mid_index - 1
+
+    return -1
+
+def find_all_occurances(numbers, number_to_find):
+    index = binary_search(numbers, number_to_find)
+    indices = [index]
+    # find indices on left hand side
+    i = index-1
+    while i >=0:
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i - 1
+
+    # find indices on right hand side
+    i = index + 1
+    while i<len(numbers):
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i + 1
+
+    return sorted(indices)
+
+
+if __name__ == '__main__':
+    numbers = [1,4,6,9,11,15,15,15,17,21,34,34,56]
+    number_to_find = 15
+    indices = find_all_occurances(numbers, number_to_find)
+    print(f"Indices of occurances of {number_to_find} are {indices}")
