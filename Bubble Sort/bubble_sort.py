e7e92f09da4143a3793a0dcacce0ed7d63756808
Dinesh <61511078+Dinesh-Sivanandam@users.noreply.github.com>
2020-10-19 13:20:33 +0530

    Bubble Sort Implementations

---
+++ Bubble Sort/bubble_sort.py
@@ -0,0 +1,23 @@
+def bubble_sort(elements):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+
+if __name__ == '__main__':
+    elements = [1,5,9,2,67,34,88,34]
+    #elements = [1,2,3,4,2]
+    #elements = ["mona", "dhaval", "aamir", "tina", "chang"]
+
+    bubble_sort(elements)
+    print(elements)
